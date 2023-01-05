b7ec5a29bcfb8a1304c18703678eced64cb20de3
Melvin Nnadi <109449499+Melsounds@users.noreply.github.com>
2023-01-05 12:19:10 -0800

    Create app2.js

---
+++ styles/app2.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let images = document.querySelectorAll(img);
+})
+
+window.addEventListener('load', () => {
+    let body = document.querySelector('body');
+})
